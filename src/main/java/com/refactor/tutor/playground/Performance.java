ed6cab3fe218961f5e5e998fc9aa2ee5147c63e0
moove <mooveprince@gmail.com>
2025-12-07 16:16:36 -0500

    moved to class which holds data

--- src/main/java/com/refactor/tutor/playground/Performance.java
+++ src/main/java/com/refactor/tutor/playground/Performance.java
@@ -20,42 +20,11 @@ public class Performance {
     }
 
     public BigDecimal amount() {
-
-        BigDecimal result;
-
-        switch (play.getType().toString().toLowerCase()) {
-            case "tragedy" -> {
-                result = BigDecimal.valueOf(40000);
-                if (audience > 30) {
-                    result = result.add(BigDecimal.valueOf(1000L * (audience - 30)));
-                }
-            }
-            case "comedy" -> {
-                result = BigDecimal.valueOf(30000);
-                if (audience > 20) {
-                    result = result.add(BigDecimal.valueOf(10000L + 500L * (audience - 20)));
-                }
-                result = result.add(BigDecimal.valueOf(300L * audience));
-            }
-            default -> throw new IllegalArgumentException("unknown type: " + play.getType());
-        }
-
-        return result;
-
+        return play.amount(audience);
     }
 
     public int volumeCredits () {
-
-        int result = 0;
-
-        // calculate volume credits
-        result += Math.max(audience - 30, 0);
-        if ("comedy".equalsIgnoreCase(play.getType().toString())) {
-            result += Math.floor(audience / 5);
-        }
-
-        return result;
-
+        return play.volumeCredits(audience);
     }
 
 }
--- src/main/java/com/refactor/tutor/playground/Play.java
+++ src/main/java/com/refactor/tutor/playground/Play.java
@@ -2,6 +2,8 @@ package com.refactor.tutor.playground;
 
 import com.refactor.tutor.model.PlayType;
 
+import java.math.BigDecimal;
+
 public class Play {
 
     String name;
@@ -18,4 +20,42 @@ public class Play {
     public PlayType getType() {
         return playType;
     }
+
+    public BigDecimal amount(int audience) {
+
+        BigDecimal result;
+
+        switch (playType.toString().toLowerCase()) {
+            case "tragedy" -> {
+                result = BigDecimal.valueOf(40000);
+                if (audience > 30) {
+                    result = result.add(BigDecimal.valueOf(1000L * (audience - 30)));
+                }
+            }
+            case "comedy" -> {
+                result = BigDecimal.valueOf(30000);
+                if (audience > 20) {
+                    result = result.add(BigDecimal.valueOf(10000L + 500L * (audience - 20)));
+                }
+                result = result.add(BigDecimal.valueOf(300L * audience));
+            }
+            default -> throw new IllegalArgumentException("unknown type: " + playType);
+        }
+
+        return result;
+
+    }
+
+    public int volumeCredits(int audience) {
+        int result = 0;
+
+        // calculate volume credits
+        result += Math.max(audience - 30, 0);
+        if ("comedy".equalsIgnoreCase(playType.toString())) {
+            result += Math.floor(audience / 5);
+        }
+
+        return result;
+    }
+
 }
